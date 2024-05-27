be3a32633de365abb04eff9ca81a6f6470079d3c
sehgxl <abhinavsehgal802@gmail.com>
2024-05-28 00:15:18 +0530

    refactor: Add constants for currency exchange API URLs

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const CURRENCY_EXCHANGE_API =
+  "https://api.freecurrencyapi.com/v1/latest?base_currency=USD";
+
+export const CURRENCY_API = "https://api.freecurrencyapi.com/v1/currencies";
